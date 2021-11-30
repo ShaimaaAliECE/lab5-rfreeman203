e0afce726f77d9a18f79ff8752b3874f23d75670
Robert <49911127+rfreeman203@users.noreply.github.com>
2021-11-30 14:15:56 -0500

    finished q1-q3

--- index.js
+++ index.js
@@ -9,46 +9,55 @@ app.use(express.static('static'));
 1-The categories mentioned in all the jobs and how many times each category was 
 mentioned.
 */
+
 app.get('/categories', (req, res) => {
-    
-    for(j in jobsList){
-        for(c in j){
-            console.log(c + " -> " + j[c]);
-        }
-    }
-    /*
-    var categoriesList = [];
+    let count = {};
 
-    for (c of jobsList.categories)
-    {
-        console.log(c);
-
-        
-        for (let c of j.categories){
-            if(c in categoriesList.categories)
-            {
-                //increase qty of category here
-                continue;
+    for (j in jobsList){
+        for(c of jobsList[j].categories){
+            if(c in count){
+                count[c]++;
             }
-            else
-            {
-                var category = {
-                    category: c,
-                    qty : 0
-                }
-
-                categoriesList.push(category)
+            else{
+                count[c] = 1;
             }
         }
-        
     }
-    */
+
+    res.send(JSON.stringify(count));
+
+})
+
+app.get('/jobs', (req, res) => {
+    let cities = {};
+
+    for (j in jobsList){
+        if(jobsList[j].title.includes(req.query.cities)){
+            cities[j] = jobsList[j];
+        }
+    }
+
+    res.send(JSON.stringify(cities));
+
 })
 
 /*
 2- All the jobs with a given category (sent as a parameter)
 */
+app.get('/:category', (req, res) => {
+    let jobs = {};
 
+    for (j in jobsList){
+        for(c of jobsList[j].categories){
+            if (req.params.category == c){
+                jobs[j] = jobsList[j];
+            }
+        }
+    }
+
+    res.send(jobs);
+
+})
 
 /*
 3- All jobs in a given city (sent in the querystring)
